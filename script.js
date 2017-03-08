587dbbcbaac296be09fc8d0aef359b2a033384dc
Rostyslav Chaikivskyi <r.chaikivskyi@gmail.com>
2017-03-08 23:33:42 +0200

    first lesson

---
+++ script.js
@@ -0,0 +1,36 @@
+// for (var mark in document) {
+//     document.write(mark + '<br>');
+// }
+// console.dir(document);
+
+// var title = document.getElementById('title');
+//
+// var links = document.getElementsByClassName('link');
+//
+// console.log(title);
+// console.log('=========');
+// console.log(links);
+
+// Виведемо тип вузла документа
+
+// console.log(document.documentElement.nodeType);
+
+// Виведемо дочірні вузли
+
+// console.log(document.body.childNodes);
+
+// var wrapper = document.getElementById('wrapper');
+// var wrapperClasses = wrapper.getAttribute('class');
+// var wrapperClasesNew = wrapperClasses + ' wrapper_class';
+// wrapper.setAttribute('class', wrapperClasesNew);
+
+// var wrapper = document.getElementById('wrapper');
+// console.log(wrapper.classList);
+// wrapper.classList.add('wrapper_class');
+// wrapper.classList.remove('example_class1');
+
+// function changeClass() {
+//     wrapper.classList.toggle('example_class2');
+// }
+
+
